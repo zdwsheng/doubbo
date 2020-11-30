309601dbe372ae9460a7758b5a22140f43a28e56
zdw19960125.@ <zhangdw1@risenenergy.com>
2020-11-30 10:58:08 +0800

    add:初次提交代码

---
+++ zdw-provider-man/src/main/java/com/zdw/provider/man/ZdwProviderManApplication.java
@@ -0,0 +1,13 @@
+package com.zdw.provider.man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdwProviderManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdwProviderManApplication.class, args);
+	}
+
+}
---
+++ zdw-provider-women/src/main/java/com/zdw/provider/women/ZdwProviderWomenApplication.java
@@ -0,0 +1,13 @@
+package com.zdw.provider.women;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdwProviderWomenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdwProviderWomenApplication.class, args);
+	}
+
+}
